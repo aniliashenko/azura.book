b8e8c45c5ad602dbf782eecb858df06b51547c3b
aniliashenko <windsweptofficial@gmail.com>
2025-03-23 23:39:33 +0100

    fixed-7

--- src/test/java/application/ApplicationTests.java
+++ src/test/java/application/ApplicationTests.java
@@ -1,13 +1,13 @@
 package application;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import application.config.AppConfig;
 import application.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Import(AppConfig.class)
