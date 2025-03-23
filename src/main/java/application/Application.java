0280896371fd596d1a90155b0d508f538faf5f1c
aniliashenko <windsweptofficial@gmail.com>
2025-03-23 18:55:22 +0100

    fixed-3

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -7,13 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-		};
-	}
+	public CommandLineRunner commandLineRunner() { return args -> {}; }
 }
