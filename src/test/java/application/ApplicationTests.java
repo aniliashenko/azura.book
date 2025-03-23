28807d5b6b52923e85dfcc791575319bd32b0164
aniliashenko <windsweptofficial@gmail.com>
2025-03-23 23:38:43 +0100

    fixed-6

--- src/test/java/application/ApplicationTests.java
+++ src/test/java/application/ApplicationTests.java
@@ -1,13 +1,22 @@
 package application;
 
+import application.config.AppConfig;
+import application.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Import(AppConfig.class)
 class ApplicationTests {
+    @Autowired
+    private BookRepository bookRepository;
 
     @Test
     void contextLoads() {
+        assertNotNull(bookRepository, "BookRepository should be initialized");
     }
-
 }
