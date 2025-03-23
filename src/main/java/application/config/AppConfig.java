c5c1e4c78968c61a333f9ebf07e681d6c7cfc020
aniliashenko <windsweptofficial@gmail.com>
2025-03-23 23:27:34 +0100

    fixed-2

--- src/main/java/application/config/AppConfig.java
+++ src/main/java/application/config/AppConfig.java
@@ -1,9 +1,6 @@
 package application.config;
 
-import java.util.Properties;
 import javax.sql.DataSource;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,26 +10,10 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 @ComponentScan(basePackages = "application")
 public class AppConfig {
     @Bean
-    public DataSource getDataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app?serverTimeZone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("12345678");
-        return dataSource;
-    }
-
-    @Bean
-    public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
-        localSessionFactoryBean.setDataSource(getDataSource());
-
-        Properties properties = new Properties();
-        properties.put(Environment.SHOW_SQL, "true");
-        properties.put(Environment.HBM2DDL_AUTO, "create-drop");
-
-        localSessionFactoryBean.setHibernateProperties(properties);
-        localSessionFactoryBean.setPackagesToScan("mate.academy.model");
-        return localSessionFactoryBean;
+    public LocalSessionFactoryBean getSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("mate.academy.model");
+        return sessionFactory;
     }
 }
